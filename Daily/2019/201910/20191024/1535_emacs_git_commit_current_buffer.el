efc0ed56671887afbc381fb63cd70cb5f8c5b63e
doneself <doneself@qq.com>
2019-10-24 16:28:41 +0800

    第25天_在页面中的应该使用奇数还是偶数的字体.md 2019-10-24 Thu 16:28:40

--- Daily/2019/201910/20191024/1535_emacs_git_commit_current_buffer.el
+++ Daily/2019/201910/20191024/1535_emacs_git_commit_current_buffer.el
@@ -4,5 +4,5 @@
   (interactive)
   (when (buffer-file-name)
 	(let* ((current-time (format-time-string "%Y-%m-%d %a %H:%M:%S"))
-		   (command-str (format "git add -A && git commit -m \"%s %s\"" (buffer-name) current-time)))
+		   (command-str (format "git add -A && git commit -m \"%s. %s\"" (buffer-name) current-time)))
 	  (async-shell-command command-str))))
