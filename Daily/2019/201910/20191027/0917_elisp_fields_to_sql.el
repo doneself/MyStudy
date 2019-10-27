1eb3bd5ecd32e9ea42dc00b054b2b3ec50acdc33
doneself <doneself@qq.com>
2019-10-27 09:43:29 +0800

    0917_elisp_fields_to_sql.el

---
+++ Daily/2019/201910/20191027/0917_elisp_fields_to_sql.el
@@ -0,0 +1,29 @@
+;;elisp fields to sql
+
+(defun fields-to-insert-sql ()
+  (interactive)
+  (require 'subr-x)
+  (let ((result-buffer-name "*Sql String*")
+		(field-list (split-string (buffer-string) "[ \f\t\n\r\v]+" t)))
+	(with-current-buffer
+		(set-buffer (get-buffer-create result-buffer-name))
+	  (erase-buffer)
+	  (insert "insert into tablename \n(")
+	(mapcar (lambda(item) (insert (format "%s, " (string-trim item)))) field-list)
+	(insert ")\n values \n(")
+	(mapcar (lambda(item) (insert (format ":%s, " (string-trim item)))) field-list)
+	(insert ")"))
+	(switch-to-buffer-other-window result-buffer-name)))
+
+
+(defun fields-to-update-sql ()
+  (interactive)
+  (require 'subr-x)
+  (let ((result-buffer-name "*Sql String*")
+		(field-list (split-string (buffer-string) "[ \f\t\n\r\v]+" t)))
+	(with-current-buffer
+		(set-buffer (get-buffer-create result-buffer-name))
+	  (erase-buffer)
+	  (insert "UPDATE tablename SET ")
+	(mapcar (lambda(item) (insert (format "%s = :%s, " (string-trim item) (string-trim item)))) field-list)
+	(switch-to-buffer-other-window result-buffer-name))))
