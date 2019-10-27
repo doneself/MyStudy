09146874a692d1429c8f9ed0142bbce1b10a3387
Lok <donself@qq.com>
2019-10-27 11:55:37 +0800

    0917_elisp_fields_to_sql.el updated.

--- Daily/2019/201910/20191027/0917_elisp_fields_to_sql.el
+++ Daily/2019/201910/20191027/0917_elisp_fields_to_sql.el
@@ -1,7 +1,7 @@
 ;;elisp fields to sql
 
-(defun fields-to-insert-sql ()
-  (interactive)
+(defun fields-to-insert-sql (select-symbol)
+  (interactive (list (completing-read "select:" (list ":" "@"))))
   (require 'subr-x)
   (let ((result-buffer-name "*Sql String*")
 		(field-list (split-string (buffer-string) "[ \f\t\n\r\v]+" t)))
@@ -12,14 +12,14 @@
 	(mapcar (lambda(item) (insert (format "%s, " (string-trim item)))) field-list)
 	(backward-delete-char-untabify 2)
 	(insert ")\n values \n(")
-	(mapcar (lambda(item) (insert (format ":%s, " (string-trim item)))) field-list)
+	(mapcar (lambda(item) (insert (format "%s%s, " select-symbol (string-trim item)))) field-list)
 	(backward-delete-char-untabify 2)
 	(insert ")"))
 	(switch-to-buffer-other-window result-buffer-name)))
 
 
-(defun fields-to-update-sql ()
-  (interactive)
+(defun fields-to-update-sql (select-symbol)
+  (interactive (list (completing-read "select:" (list ":" "@"))))
   (require 'subr-x)
   (let ((result-buffer-name "*Sql String*")
 		(field-list (split-string (buffer-string) "[ \f\t\n\r\v]+" t)))
@@ -27,6 +27,6 @@
 		(set-buffer (get-buffer-create result-buffer-name))
 	  (erase-buffer)
 	  (insert "UPDATE tablename SET ")
-	  (mapcar (lambda(item) (insert (format "%s = :%s, " (string-trim item) (string-trim item)))) field-list)
+	  (mapcar (lambda(item) (insert (format "%s = %s%s, " (string-trim item) select-symbol (string-trim item)))) field-list)
 	  (backward-delete-char-untabify 2)
 	  (switch-to-buffer-other-window result-buffer-name))))
