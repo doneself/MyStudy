12921dd1d2662758260cd7342892aed419cdd052
doneself <doneself@qq.com>
2019-12-04 20:03:31 +0800

    1607_emacs_lisp_遍历文件夹所有文本.el updated.

--- Daily/2019/201912/20191204/1607_emacs_lisp_遍历文件夹所有文本.el
+++ Daily/2019/201912/20191204/1607_emacs_lisp_遍历文件夹所有文本.el
@@ -1,3 +1,7 @@
 emacs lisp 遍历文件夹所有文本
 
 (mapcar (lambda(item) (insert (format "\n%s" item))) (directory-files-recursively "d:/Zero/MyDocument/Note/" "[(\\.txt$)(\\.org$)]"))
+
+(rx "." (or "org" "md" "txt"))
+
+(mapcar (lambda(item) (insert (format "\n%s" item)))(directory-files-recursively "D:/ZerOne/MyMemo" "\\.\\(?:org\\|txt\\)$"))
