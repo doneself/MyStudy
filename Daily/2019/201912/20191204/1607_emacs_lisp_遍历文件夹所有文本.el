1dbd9ccd7ae2be5e5f069495fe8e96a7683971dc
doneself <doneself@qq.com>
2019-12-04 16:33:39 +0800

    1607_emacs_lisp_遍历文件夹所有文本.el Updated.

--- Daily/2019/201912/20191204/1607_emacs_lisp_遍历文件夹所有文本.el
+++ Daily/2019/201912/20191204/1607_emacs_lisp_遍历文件夹所有文本.el
@@ -1,3 +1,3 @@
 emacs lisp 遍历文件夹所有文本
 
-(mapcar (lambda(item) (insert (format "\n%s" item))) (directory-files-recursively "d:/Zero/MyDocument/Note/" "\\.txt$"))
+(mapcar (lambda(item) (insert (format "\n%s" item))) (directory-files-recursively "d:/Zero/MyDocument/Note/" "[(\\.txt$)(\\.org$)]"))
