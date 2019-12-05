d622709ca6f07c670fea477839d82d2c7bf8b338
doneself <doneself@qq.com>
2019-12-05 11:27:37 +0800

    1607_emacs_lisp_遍历文件夹所有文本.el updated.

--- Daily/2019/201912/20191204/1607_emacs_lisp_遍历文件夹所有文本.el
+++ Daily/2019/201912/20191204/1607_emacs_lisp_遍历文件夹所有文本.el
@@ -24,3 +24,22 @@ emacs lisp 遍历文件夹所有文本
 	  (goto-char (point-min))
 	  (display-line-numbers-mode t))
 	(switch-to-buffer-other-window note-list-buffer-name)))
+
+(defun get-directory-note-file (dir regexp-input)
+  (interactive
+   (list (read-directory-name "选择路径：")
+		 (read-string "输入搜索正则，默认(txt|org|md) : ")))
+  (require 'subr-x)
+  (let* ((note-list-buffer-name "*Note List*")
+		 (regexp-string (if (string-blank-p regexp-input)
+							(rx "." (or "org" "txt" "md"))
+						  regexp-input)))
+	(with-current-buffer
+		(set-buffer (get-buffer-create note-list-buffer-name))
+	  (erase-buffer)
+	  (insert (format "Search time: %s\n" (format-time-string "%Y-%m-%d %a %H:%M:%S")))
+	  (mapcar (lambda(item) (insert (format "\n%s" item)))
+			  (directory-files-recursively dir regexp-string))
+	  (goto-char (point-min))
+	  (display-line-numbers-mode t))
+	(switch-to-buffer-other-window note-list-buffer-name)))
