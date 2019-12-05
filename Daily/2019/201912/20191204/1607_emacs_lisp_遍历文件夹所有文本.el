7dabbdcdf0c2c44b176bcd2b20c05d06231948f8
doneself <doneself@qq.com>
2019-12-05 08:58:10 +0800

    1607_emacs_lisp_遍历文件夹所有文本.el updated.

--- Daily/2019/201912/20191204/1607_emacs_lisp_遍历文件夹所有文本.el
+++ Daily/2019/201912/20191204/1607_emacs_lisp_遍历文件夹所有文本.el
@@ -5,3 +5,22 @@ emacs lisp 遍历文件夹所有文本
 (rx "." (or "org" "md" "txt"))
 
 (mapcar (lambda(item) (insert (format "\n%s" item)))(directory-files-recursively "D:/ZerOne/MyMemo" "\\.\\(?:org\\|txt\\)$"))
+
+
+;;read-directory-name
+(read-directory-name "选择路径：")
+
+
+(defun get-directory-note-file (dir)
+  (interactive
+   (list (read-directory-name "选择路径：")))
+  (let* ((note-list-buffer-name "*Note List*"))
+	(with-current-buffer
+		(set-buffer (get-buffer-create note-list-buffer-name))
+	  (erase-buffer)
+	  (insert (format "Search time: %s\n" (format-time-string "%Y-%m-%d %a %H:%M:%S")))
+	  (mapcar (lambda(item) (insert (format "\n%s" item)))
+			  (directory-files-recursively dir (rx "." (or "org" "txt" "md"))))
+	  (goto-char (point-min))
+	  (display-line-numbers-mode t))
+	(switch-to-buffer-other-window note-list-buffer-name)))
