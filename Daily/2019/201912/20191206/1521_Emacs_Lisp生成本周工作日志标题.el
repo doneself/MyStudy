801c32894c354d363bf89ab3f47295125caf60ff
doneself <doneself@qq.com>
2019-12-06 15:27:00 +0800

    1521_Emacs_Lisp生成本周工作日志标题.el

---
+++ Daily/2019/201912/20191206/1521_Emacs_Lisp生成本周工作日志标题.el
@@ -0,0 +1,16 @@
+;;Emacs Lisp生成本周工作日志标题
+
+(defun insert-workreport-file-name ()
+  (interactive)
+  (let* ((current-time (decode-time))
+	   (day (nth 3 current-time))
+	   (month (nth 4 current-time))
+	   (weekday (nth 6 current-time))
+	   (dis-monday (- 1 weekday))
+	   (day-seconds 86400)
+	   (monday-time (time-add (current-time) (seconds-to-time (* day-seconds dis-monday))))
+	   (firday-time (time-add monday-time (seconds-to-time (* day-seconds 4))))
+	   )
+  (insert (format "本周工作报告%s~%s.txt"
+				  (format-time-string "%m-%d" monday-time)
+				  (format-time-string "%m-%d" firday-time)))))
