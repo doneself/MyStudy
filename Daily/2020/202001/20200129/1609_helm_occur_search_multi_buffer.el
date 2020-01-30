ad85091e45f114d8076838ebda6757f9c0c4ab8c
doneself <doneself@qq.com>
2020-01-30 09:58:26 +0800

    helm buffer list filter.

--- Daily/2020/202001/20200129/1609_helm_occur_search_multi_buffer.el
+++ Daily/2020/202001/20200129/1609_helm_occur_search_multi_buffer.el
@@ -1,5 +1,42 @@
 ;;helm occur search multi buffer
 
+;;;2020-01-30 Thu 09:56:51
+(defun helm-search-text-in-all-buffers()
+  (interactive)
+  (require 'cl)
+  (require 'helm-occur)
+  (unless (region-active-p)
+	(helm-multi-occur-1
+	 (cl-delete-if
+	  (lambda (item)
+		(or
+		 (string-prefix-p " *Minibuf-" (buffer-name item))
+		 (string-prefix-p " *which-key" (buffer-name item))
+		 (string-prefix-p " *Echo" (buffer-name item))
+		 (string-prefix-p " *helm" (buffer-name item))
+		 (string-prefix-p "*helm" (buffer-name item))
+		 (string-prefix-p " *code-conversion" (buffer-name item))
+		 (string-prefix-p " *urlparse" (buffer-name item))
+		 )) (buffer-list))
+	 ))
+  (when (region-active-p)
+	(helm-multi-occur-1
+	 ;;(cl-delete-if (lambda (buffer) (find (aref (buffer-name buffer) 0) " *")) (buffer-list))
+	 (cl-delete-if
+	  (lambda (item)
+		(or
+		 (string-prefix-p " *Minibuf-" (buffer-name item))
+		 (string-prefix-p " *which-key" (buffer-name item))
+		 (string-prefix-p " *Echo" (buffer-name item))
+		 (string-prefix-p " *helm" (buffer-name item))
+		 (string-prefix-p "*helm" (buffer-name item))
+		 (string-prefix-p " *code-conversion" (buffer-name item))
+		 (string-prefix-p " *urlparse" (buffer-name item))
+		 )) (buffer-list))
+	 (buffer-substring-no-properties (region-beginning) (region-end)))))
+
+
+;;2020-01-29 Wed 09:57:15
 (defun helm-search-text-in-all-buffers()
   (interactive)
   (require 'cl)
