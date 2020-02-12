bc510c247b60bd775fd81c546d47b429ec03a3fd
doneself <doneself@qq.com>
2020-02-12 22:48:51 +0800

    2148_emacs_time_practice.el

--- Daily/2020/202002/20200212/2148_emacs_time_practice.el
+++ Daily/2020/202002/20200212/2148_emacs_time_practice.el
@@ -36,12 +36,30 @@
 
 (defun clear-yesterday-before-file (dir)
   (let ((file-list (directory-files-recursively dir (rx anything)))
-		)
+		 )
 	(mapcar (lambda(item)
-			  (if (yesterday-before-file-p item)
+			  (when (yesterday-before-file-p item)
 				  (delete-file item))
 			  ) file-list)))
 
 
 (clear-yesterday-before-file "G:/OldFiles/201902/打印图片")
 (clear-yesterday-before-file "C:/Users/lok/Desktop/Daily")
+
+
+(defun clear-yesterday-before-file (dir)
+  (let* ((file-list (directory-files-recursively dir (rx anything)))
+	   (now (decode-time))
+	   (minute (nth 1 now))
+	   (hour (nth 2 now))
+	   (time-seconds (- 0 (* 60 minute) (* 60 60 hour)))
+	   (time-zero (time-add (current-time) (seconds-to-time time-seconds)))
+	   )
+	(mapcar (lambda(item)
+			  (if (time-less-p
+					 (file-attribute-modification-time (file-attributes item)) time-zero)
+				  (delete-file item))
+			  ) file-list)))
+
+(current-time)
+(24132 3803 273829 0)
